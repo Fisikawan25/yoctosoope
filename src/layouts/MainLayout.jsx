6fe82ce7220d117dfe397e89f86fa25c9184c147
Mahsa Abhirama Putra Ramadhan <196095091+Fisikawan25@users.noreply.github.com>
2025-12-31 06:36:25 +0700

    Update import path for useToast context

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
-import { useToast } from '../context/ToastContext';
+import { useToast } from '@context';
 import '../assets/styles/global.css'; // Memastikan variabel warna & font global termuat
 
 const MainLayout = () => {
