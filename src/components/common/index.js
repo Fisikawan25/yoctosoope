82790a9fd35b594039678b47843697c75b7b5f97
Mahsa Abhirama Putra Ramadhan <196095091+Fisikawan25@users.noreply.github.com>
2025-12-30 19:02:44 +0700

    Update index.js

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,4 @@
-export { useScroll } from './useScroll';
-export { useTheme } from './useTheme';
-export { useTheme } from './useTheme';
+export { CodeBlock } from './CodeBlock';
+export { SEOHead } from './SEOHead';
+export { LazyImage } from './LazyImage';
+
