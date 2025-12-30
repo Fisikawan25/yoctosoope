81856ec4f03698d54c926203e2333084b420eb32
Mahsa Abhirama Putra Ramadhan <196095091+Fisikawan25@users.noreply.github.com>
2025-12-30 19:33:05 +0700

    Update index.js

--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -1,3 +1,5 @@
 export { formatDate, formatTime, formatCurrency } from './formatters';
 export { validateEmail, validatePhone } from './validators';
 export { debounce, throttle, capitalize } from './helpers';
+export { EQUATION_DATA, truncateText } from './stringhelpers';
+
