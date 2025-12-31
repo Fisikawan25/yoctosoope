a71f55502a6f84c8d64de29af8d0979a0aea649d
Mahsa Abhirama Putra Ramadhan <196095091+Fisikawan25@users.noreply.github.com>
2025-12-31 07:02:40 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; 
-import AppRoutes from './routes/AppRoutes';
-import { ToastProvider } from './context/ToastContext'; 
+import { AppRoutes } from '@/routes/AppRoutes';
+import { ToastProvider } from '@/context/ToastContext'; 
 import './assets/styles/variables.css'; 
 import './assets/styles/typography.css'; 
 import './assets/styles/global.css';     // Pastikan urutan: var -> typo -> global
